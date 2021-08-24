f9e88e1f6a60474649824c460af975963eed6db0
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-08-24 22:36:09 +0300

    make buttons separate components

---
+++ src/components/elements/AppButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    type="button"
+    class="
+      inline-flex
+      items-center
+      py-2
+      px-4
+      border border-transparent
+      shadow-sm
+      text-sm
+      leading-4
+      font-medium
+      rounded-full
+      text-white
+      bg-gray-600
+      hover:bg-gray-700
+      transition-colors
+      duration-300
+      focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500
+    "
+  >
+    <slot name="content"></slot>
+  </button>
+</template>
